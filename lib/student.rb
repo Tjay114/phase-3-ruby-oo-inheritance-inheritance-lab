18e68a3c24d2beb2122c6f3113e588ee6d36f6c2
Terormumuli <terahmumuli@gmail.com>
2022-08-24 13:10:06 +0300

    Inheritance Inheritance

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+class Student < User
 
+    attr_accessor :first_name, :last_name, :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+
+    def first_name
+        "Steve"
+    end
+
+    def last_name
+        "Jobs"
+    end
 end
---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+class User
+
+    attr_accessor :first_name, :last_name
+
+    def first_name
+        "Tracy"
+    end
+
+    def last_name
+        "Morgan"
+    end
+
+
+end
